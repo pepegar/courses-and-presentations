f9750a9768fabae7873b34496b099fe70a648d7a
Pepe García <pepe@pepegar.com>
2022-09-16 17:16:49 +0100

    build slides with nix

--- flake.nix
+++ flake.nix
@@ -16,40 +16,67 @@
   outputs = { self, nixpkgs, flake-utils, nix-pandoc }:
     let
       system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
 
+      texlive-packages = {
+        inherit (pkgs.texlive)
+          scheme-small noto mweights cm-super cmbright fontaxes beamer
+          fvextra catchfile xstring framed;
+      };
+
+      texlive-combined = pkgs.texlive.combine texlive-packages;
       pandocOpts = ''
-      --variable monofont="PragmataPro Mono" \
-      --variable fontsize=12pt \
-      --variable theme=Madrid \
       --include-in-header=./style.tex \
+      --pdf-engine-opt=-output-directory=_output \
+      --pdf-engine-opt=-shell-escape \
+      --pdf-engine=xelatex \
       --standalone \
+      --variable theme=Madrid \
       -t beamer \
-      --highlight-style tango \
       '';
 
       buildSlides = title : system : nix-pandoc.mkDoc.${system} {
         name = title;
         src = ./.;
+        inherit texlive-combined;
         phases = [ "unpackPhase" "buildPhase" "installPhase" ];
         buildPhase = "pandoc ${pandocOpts} -o $name.pdf ./slides-md/$name.md";
         installPhase = "mkdir -p $out; cp $name.pdf $out";
+        extraBuildInputs = [
+          pkgs.which
+        ];
       };
     in {
       packages = {
-        ${system}."00-intro" = buildSlides "00-intro";
+        ${system} = {
+          "pt/00-intro" = buildSlides "00-intro" system;
+          "pt/01-hardware" = buildSlides "01-hardware" system;
+          "pt/02-datatypes-operators" = buildSlides "02-datatypes-operators" system;
+          "pt/03-booleans-control-flow" = buildSlides "03-booleans-control-flow" system;
+          "pt/04-lists-iteration" = buildSlides "04-lists-iteration" system;
+          "pt/05-dictionaries" = buildSlides "05-dictionaries" system;
+        };
       };
     } // flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        texlive-packages = {
+          inherit (pkgs.texlive)
+          scheme-small noto mweights cm-super cmbright fontaxes beamer minted
+          fvextra catchfile xstring framed;
+        };
+
+        texlive-combined = pkgs.texlive.combine texlive-packages;
       in rec {
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.pandoc
             pkgs.entr
             pkgs.hugo
-            pkgs.texlive.combined.scheme-full
             pkgs.python38
             pkgs.gnumake
+            texlive-combined
           ];
         };
       }
