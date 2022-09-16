8e90859cf46c31d4d44d386b9732974878c139ab
Pepe García <pepe@pepegar.com>
2022-09-16 17:47:57 +0100

    make packages work for all systems

--- flake.nix
+++ flake.nix
@@ -18,39 +18,39 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, nix-pandoc, pre-commit-hooks }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      texlive-packages = {
-        inherit (pkgs.texlive)
-          scheme-small noto mweights cm-super cmbright fontaxes beamer fvextra
-          catchfile xstring framed;
-      };
-
-      texlive-combined = pkgs.texlive.combine texlive-packages;
-      pandocOpts = ''
-        --include-in-header=./style.tex \
-        --pdf-engine-opt=-output-directory=_output \
-        --pdf-engine-opt=-shell-escape \
-        --pdf-engine=xelatex \
-        --standalone \
-        --variable theme=Madrid \
-        -t beamer \
-      '';
-
-      buildSlides = name: system:
-        nix-pandoc.mkDoc.${system} {
-          src = ./.;
-          inherit texlive-combined name;
-          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-          buildPhase = "pandoc ${pandocOpts} -o $name.pdf ./slides-md/$name.md";
-          installPhase = "mkdir -p $out; cp $name.pdf $out";
-          extraBuildInputs = [ pkgs.which ];
+        texlive-packages = {
+          inherit (pkgs.texlive)
+            scheme-small noto mweights cm-super cmbright fontaxes beamer fvextra
+            catchfile xstring framed;
         };
-    in {
-      packages = {
-        ${system} = {
+
+        texlive-combined = pkgs.texlive.combine texlive-packages;
+        pandocOpts = ''
+          --include-in-header=./style.tex \
+          --pdf-engine-opt=-output-directory=_output \
+          --pdf-engine-opt=-shell-escape \
+          --pdf-engine=xelatex \
+          --standalone \
+          --variable theme=Madrid \
+          -t beamer \
+        '';
+
+        buildSlides = name: system:
+          nix-pandoc.mkDoc.${system} {
+            src = ./.;
+            inherit texlive-combined name;
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase =
+              "pandoc ${pandocOpts} -o $name.pdf ./slides-md/$name.md";
+            installPhase = "mkdir -p $out; cp $name.pdf $out";
+            extraBuildInputs = [ pkgs.which ];
+          };
+      in rec {
+        packages = flake-utils.lib.flattenTree {
           "pt/00-intro" = buildSlides "00-intro" system;
           "pt/01-hardware" = buildSlides "01-hardware" system;
           "pt/02-datatypes-operators" =
@@ -60,19 +60,6 @@
           "pt/04-lists-iteration" = buildSlides "04-lists-iteration" system;
           "pt/05-dictionaries" = buildSlides "05-dictionaries" system;
         };
-      };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        texlive-packages = {
-          inherit (pkgs.texlive)
-            scheme-small noto mweights cm-super cmbright fontaxes beamer minted
-            fvextra catchfile xstring framed;
-        };
-
-        texlive-combined = pkgs.texlive.combine texlive-packages;
-      in rec {
         checks = {
           pre-commit-check = pre-commit-hooks.lib.${system}.run {
             src = ./.;
